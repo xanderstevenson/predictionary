32a5aa45c087710bea49af88b60d4b91ac15b6c6
Alexander <alexstev@cisco.com>
2021-10-23 17:20:26 -0500

    formatted country percentages to decimal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import requests
 
 
-
-
 # Ask the user to enter their first name and store in variable
 first_name = input("\n\nPlease enter your first name: \n\n")
 
@@ -14,6 +12,8 @@ nation_results = r.json()
 
 # print results using f-Strings
 print(f"\n\nHere are your results, {first_name}:\n")
+print('--------------------------------------\n')
+print('NATIONALITY\n\n')
 
 
 for key, item in nation_results.items():
@@ -22,9 +22,9 @@ for key, item in nation_results.items():
         country_2 = list(item[1].values())
         country_3 = list(item[2].values())
 
-print(f"Country 1: {country_1[0]}\nProbabilty: {country_1[1]}\n")
-print(f"Country 2: {country_2[0]}\nProbabilty: {country_2[1]}\n")
-print(f"Country 3: {country_3[0]}\nProbabilty: {country_3[1]}\n")
+print(f"Country 1 : {country_1[0]}\nProbabilty: {'{:.2%}'.format(country_1[1])}\n")
+print(f"Country 2 : {country_2[0]}\nProbabilty: {'{:.2%}'.format(country_2[1])}\n")
+print(f"Country 3 : {country_3[0]}\nProbabilty: {'{:.2%}'.format(country_3[1])}\n")
 
 # print(nation_results)
 
