da9efa283322c81f3ccd3a5b12a81aa3fc9458ef
Alexander <wisdom8@protonmail.com>
2021-10-25 11:44:15 -0400

    after Pi hardware failure, redid this age module

--- age.py
+++ age.py
@@ -2,7 +2,17 @@
 #     AGIFY.IO      #
 #####################
 
+import requests
+
+
+
 class Age_Class:
     
-    def age_func(name2):
-        print(f"\n{name2} is cool!")
+    def age_func(valid_name):
+        a = requests.get("https://api.agify.io?name=" + valid_name)
+        age_results = a.json()
+        age = age_results['age']
+        print('-------------------------------------------------------')
+        print(f'Based on your name, your age is predicted to be: {age}')
+        print('-------------------------------------------------------')
+
