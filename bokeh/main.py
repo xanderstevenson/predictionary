5364a2229de791741940fde110e9fd8716320c26
Alexander <wisdom8@protonmail.com>
2021-10-26 18:06:15 -0400

    bokeh templating

---
+++ bokeh/main.py
@@ -0,0 +1,49 @@
+import bokeh
+from bokeh.plotting import figure, show
+from bokeh.layouts import layout
+from bokeh.models import Div, RangeSlider, Spinner, Button, CustomJS
+
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y = [4, 5, 5, 7, 2]
+
+# create plot with circle glyphs
+p = figure(x_range=(1, 9), width=500, height=250)
+points = p.circle(x=x, y=y, size=30, fill_color="#21a7df")
+
+# set up textarea (div)
+div = Div(
+    text="""
+          <p>Select the circle's size using this control element:</p>
+          """,
+    width=200,
+    height=30,
+)
+
+# set up spinner
+spinner = Spinner(
+    title="Circle size",
+    low=0,
+    high=60,
+    step=5,
+    value=points.glyph.size,
+    width=200,
+)
+spinner.js_link("value", points.glyph, "size")
+
+div2 = Div(
+    text="""Your <a href="https://en.wikipedia.org/wiki/HTML">HTML</a>-supported text is initialized with the <b>text</b> argument.  The
+remaining div arguments are <b>width</b> and <b>height</b>. For this example, those values
+are <i>200</i> and <i>100</i>, respectively. """,
+width=400, height=100)
+
+
+layout = layout(
+    [
+        [div2],
+        [p],
+        [div, spinner],
+    ]
+)
+
+show(layout)
